5f92fd4d738893560f136d67c18e0593def870c8
YumGC <yumaigar1303@gmail.com>
2024-07-30 01:58:39 +0100

    Creando el index

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proyecto Manhattan</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<style>
+    .Titulo{
+        display: flex;
+        justify-content: center;
+    }
+</style>
+<body>
+    <header>
+        <h1 class="Titulo">Actual Shuffle For Spotify</h1>
+        <nav class="Titulo">
+            <ul>
+                <li><a href="#inicio">Inicio</a></li>
+                <li><a href="register.php">Registrarse</a></li>
+                <li><a href="login.php">Inciar Sesión</a></li>
+                <li><a href="#contacto">Contacto</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div class="Titulo">
+        <aside class="sidebar">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTd21I1HTEY0lkZBCGbWKfIZFdd1Za086ZRog&s" alt="Publicidad 1">
+        </aside>
+       
+
+</body>
+
+</html>
